c0e619066ab51ccdda1fd46edcf1df190f350074
Kirill Maiantsev <kirill.mayantsev@gmail.com>
2018-12-18 18:39:07 +0100

    Add pre-defined scenarios

--- R/app.R
+++ R/app.R
@@ -6,7 +6,7 @@ library(R6)
 GameOfLife = R6Class("GameOfLife",
   public = list(
     ## Initialize function
-    initialize = function(n_row, n_col) {
+    initialize = function(n_row, n_col, square_ratio, cyclic_border) {
       stopifnot(is.numeric(n_row), length(n_row) == 1);
       stopifnot(is.numeric(n_col), length(n_col) == 1);
       
@@ -19,11 +19,15 @@ GameOfLife = R6Class("GameOfLife",
           dimnames = list(seq_len(n_row), seq_len(n_col))
         );
       private$board_plot = ggplot() +
+        theme_bw() +
         guides(fill = FALSE) +
         scale_x_continuous(name = "x") + scale_y_continuous(name = "y") +
-        scale_fill_manual(values = c("red", "green")) +
-        coord_equal(ratio = 1);
+        scale_fill_manual(values = c("red", "green"));
+      if (square_ratio) {
+        private$board_plot = private$board_plot + coord_equal(ratio = 1);
+      }
       private$generation_index = 0;
+      private$cyclic_border = cyclic_border;
     },
     ## Override default 'print' behavior
     print = function(...) {
@@ -33,6 +37,8 @@ GameOfLife = R6Class("GameOfLife",
       return(invisible(self));
     },
     set_init_state = function(pattern = "random") {
+      private$board_mat[] = 0;
+      
       if (pattern == "random") {
         private$board_mat[] = rbinom(private$n_row * private$n_col, 1, 0.15);
       }
@@ -43,6 +49,21 @@ GameOfLife = R6Class("GameOfLife",
               c(0, 0, 1, 1, 0, 1, 0, 1, 1),
               nrow = 3, ncol = 3, byrow = TRUE
             );
+        }
+      }
+      if (pattern == "ten_cell_row") {
+        if (private$n_row >= 20 && private$n_col >= 20) {
+          half_n_row = private$n_row %/% 2;
+          half_n_col = private$n_col %/% 2;
+          private$board_mat[half_n_row, (half_n_col - 4):(half_n_col + 5)] = 1;
+        }
+      }
+      if (pattern == "exploder") {
+        if (private$n_row >= 20 && private$n_col >= 20) {
+          half_n_row = private$n_row %/% 2;
+          half_n_col = private$n_col %/% 2;
+          private$board_mat[(half_n_row - 2):(half_n_row + 2), c(half_n_col - 2, half_n_col + 2)] = 1;
+          private$board_mat[c(half_n_row - 2, half_n_row + 2), half_n_col] = 1;
         }
       }
       
@@ -64,7 +85,7 @@ GameOfLife = R6Class("GameOfLife",
       out_board_plot = private$board_plot +
         geom_rect(
           data = board_df,
-          mapping = aes(xmin = x - 1, xmax = x, ymin = y - 1, ymax = y, fill = as.factor(status)),
+          mapping = aes(xmin = y - 1, xmax = y, ymin = x - 1, ymax = x, fill = as.factor(status)),
           color = "black"
         )
       
@@ -82,6 +103,7 @@ GameOfLife = R6Class("GameOfLife",
     board_mat = NULL,
     board_plot = NULL,
     generation_index = NULL,
+    cyclic_border = NULL,
     # Private functions
     obtain_neighbors_amount = function(status_mat) {
       stopifnot(min(status_mat) >= 0, max(status_mat) <= 1);
@@ -114,37 +136,50 @@ GameOfLife = R6Class("GameOfLife",
       extended_neighbors_mat[row_index_vec + 1, col_index_vec + 1] =
         extended_neighbors_mat[row_index_vec + 1, col_index_vec + 1] + status_mat;
       
-      ## compress extended matrix
-      ## borders without corners
-      min_row_ind = min(row_index_vec);
-      max_row_ind = max(row_index_vec);
-      min_col_ind = min(col_index_vec);
-      max_col_ind = max(col_index_vec);
-      extended_neighbors_mat[max_row_ind, col_index_vec] =
-        extended_neighbors_mat[max_row_ind, col_index_vec] + extended_neighbors_mat[1, col_index_vec];
-      extended_neighbors_mat[min_row_ind, col_index_vec] =
-        extended_neighbors_mat[min_row_ind, col_index_vec] + extended_neighbors_mat[n_ext_row, col_index_vec];
-      extended_neighbors_mat[row_index_vec, max_col_ind] =
-        extended_neighbors_mat[row_index_vec, max_col_ind] + extended_neighbors_mat[row_index_vec, 1];
-      extended_neighbors_mat[row_index_vec, min_col_ind] =
-        extended_neighbors_mat[row_index_vec, min_col_ind] + extended_neighbors_mat[row_index_vec, n_ext_col];
-      ## corners
-      lower_dim = min(n_input_row, n_input_col);
-      inv_lower_dim_row = n_input_row - lower_dim + 1;
-      inv_lower_dim_col = n_input_col - lower_dim + 1;
-      ext_row_ind = row_index_vec[lower_dim];
-      inv_ext_row_ind = row_index_vec[inv_lower_dim_row];
-      ext_col_ind = col_index_vec[lower_dim];
-      inv_ext_col_ind = col_index_vec[inv_lower_dim_col];
-      
-      extended_neighbors_mat[ext_row_ind, ext_col_ind] =
-        extended_neighbors_mat[ext_row_ind, ext_col_ind] + extended_neighbors_mat[1, 1];
-      extended_neighbors_mat[inv_ext_row_ind, ext_col_ind] =
-        extended_neighbors_mat[inv_ext_row_ind, ext_col_ind] + extended_neighbors_mat[n_ext_row, 1];
-      extended_neighbors_mat[ext_row_ind, inv_ext_col_ind] =
-        extended_neighbors_mat[ext_row_ind, inv_ext_col_ind] + extended_neighbors_mat[1, n_ext_col];
-      extended_neighbors_mat[inv_ext_row_ind, inv_ext_col_ind] =
-        extended_neighbors_mat[inv_ext_row_ind, inv_ext_col_ind] + extended_neighbors_mat[n_ext_row, n_ext_col];
+      if (private$cyclic_border) {
+        ## take into account borders relationship in case of cyclic borders
+        
+        ## borders without corners
+        min_row_ind = min(row_index_vec);
+        max_row_ind = max(row_index_vec);
+        min_col_ind = min(col_index_vec);
+        max_col_ind = max(col_index_vec);
+        
+        extended_neighbors_mat[max_row_ind, col_index_vec] =
+          extended_neighbors_mat[max_row_ind, col_index_vec] +
+          extended_neighbors_mat[1, col_index_vec];
+        extended_neighbors_mat[min_row_ind, col_index_vec] =
+          extended_neighbors_mat[min_row_ind, col_index_vec] +
+          extended_neighbors_mat[n_ext_row, col_index_vec];
+        extended_neighbors_mat[row_index_vec, max_col_ind] =
+          extended_neighbors_mat[row_index_vec, max_col_ind] +
+          extended_neighbors_mat[row_index_vec, 1];
+        extended_neighbors_mat[row_index_vec, min_col_ind] =
+          extended_neighbors_mat[row_index_vec, min_col_ind] +
+          extended_neighbors_mat[row_index_vec, n_ext_col];
+        
+        ## corners
+        lower_dim = min(n_input_row, n_input_col);
+        inv_lower_dim_row = n_input_row - lower_dim + 1;
+        inv_lower_dim_col = n_input_col - lower_dim + 1;
+        ext_row_ind = row_index_vec[lower_dim];
+        inv_ext_row_ind = row_index_vec[inv_lower_dim_row];
+        ext_col_ind = col_index_vec[lower_dim];
+        inv_ext_col_ind = col_index_vec[inv_lower_dim_col];
+        
+        extended_neighbors_mat[ext_row_ind, ext_col_ind] =
+          extended_neighbors_mat[ext_row_ind, ext_col_ind] +
+          extended_neighbors_mat[1, 1];
+        extended_neighbors_mat[inv_ext_row_ind, ext_col_ind] =
+          extended_neighbors_mat[inv_ext_row_ind, ext_col_ind] +
+          extended_neighbors_mat[n_ext_row, 1];
+        extended_neighbors_mat[ext_row_ind, inv_ext_col_ind] =
+          extended_neighbors_mat[ext_row_ind, inv_ext_col_ind] +
+          extended_neighbors_mat[1, n_ext_col];
+        extended_neighbors_mat[inv_ext_row_ind, inv_ext_col_ind] =
+          extended_neighbors_mat[inv_ext_row_ind, inv_ext_col_ind] +
+          extended_neighbors_mat[n_ext_row, n_ext_col];
+      }
       
       return(extended_neighbors_mat[row_index_vec, col_index_vec]);
     },
@@ -184,12 +219,17 @@ ui = fluidPage(
       sliderInput("ncol", "Cols number:", value = 20, min = 1, max = 100),
       sliderInput("delay", "Animation delay (seconds):",
                   value = 1, min = 0.2, max = 2, step = 0.2),
-      radioButtons(
+      checkboxInput("square_ratio", "Square game board"),
+      checkboxInput("cyclic_border", "Cyclic game borders"),
+      selectizeInput(
         "pattern", "Game pattern:",
-        c(
-          "Random" = "random",
-          "Glider" = "glider"
-        )
+        choices =
+          c(
+            "Random" = "random",
+            "Glider" = "glider",
+            "10 Cell Row" = "ten_cell_row",
+            "Exploder" = "exploder"
+          )
       ),
       actionButton("create", "Create"),
       actionButton("init", "Init"),
@@ -198,7 +238,7 @@ ui = fluidPage(
     ),
     
     mainPanel(
-      plotOutput("board_plot")
+      plotOutput("board_plot", width = "100%", height = "600px")
     )
   )
 )
@@ -224,19 +264,27 @@ server = function(input, output) {
   });
   
   observeEvent(input$create, {
-    new_game <<- GameOfLife$new(n_row = input$nrow, n_col = input$ncol);
+    new_game <<-
+      GameOfLife$new(
+        n_row = input$nrow,
+        n_col = input$ncol,
+        square_ratio = input$square_ratio,
+        cyclic_border = input$cyclic_border
+      );
     game_reactive$board_ggplot_obj = new_game$plot_game_board();
     new_game$print();
   });
   
   observeEvent(input$init, {
-    new_game$set_init_state(pattern = input$pattern);
-    game_reactive$board_ggplot_obj = new_game$plot_game_board();
-    new_game$print();
+    if (!is.null(new_game)) {
+      new_game$set_init_state(pattern = input$pattern);
+      game_reactive$board_ggplot_obj = new_game$plot_game_board();
+      new_game$print();
+    }
   });
   
   observeEvent(input$start, {
-    if (is.null(game_reactive$timer)) {
+    if (!is.null(new_game) && is.null(game_reactive$timer)) {
       game_reactive$timer = reactiveTimer(1000 * input$delay)
     }
   });
